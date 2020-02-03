0f3a1fa20a8c71d13f3d726989c4120957468978
Annie <anniezhao107@gmail.com>
2020-02-03 15:57:24 -0800

    keeping metadata, removing scripts

--- scraper.py
+++ scraper.py
@@ -29,8 +29,12 @@ def extract_next_links(url, resp):
 
 def getText(url):
     rawHtml = urlopen(url).read().decode("utf-8") #Gets the string of the entire html document
-    tags = re.compile(r"<.*>")
-    noTagsString = re.sub(tags," ",rawHtml)
+    tags = re.compile(r"<script.*<\/script>")  
+    noTagsString = re.sub(tags," ",rawHtml) #Scripts hold undesired data
+    tags = re.compile(r'<meta .*content="')
+    noTagsString = re.sub(tags," ",noTagsString)
+    tags = re.compile(r"<.*>")      #Remove all tags
+    noTagsString = re.sub(tags," ",noTagsString)
     print(noTagsString)
 
 
