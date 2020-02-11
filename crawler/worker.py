1d03e272e1a3d93554acd94cd209a7a6c92d1e08
Annie <anniezhao107@gmail.com>
2020-02-10 19:54:16 -0800

    removing running variable

--- crawler/worker.py
+++ crawler/worker.py
@@ -18,54 +18,52 @@ class Worker(Thread):
         self.workers = 0    #All the workers
         self.myBackupList= list()      #List of urls belonging to this worker
         self.worker_id = worker_id
-        self.running = True
         super().__init__(daemon=True)
         
     def run(self):
-        while True:
-            while(self.running):
-                if len(self.myBackupList) == 0:
-                    #No more worker urls, search frontier
-                    tbd_url = self.frontier.get_tbd_url()
-                    if not tbd_url:
+        while True:            
+            if len(self.myBackupList) == 0:
+                #No more worker urls, search frontier
+                tbd_url = self.frontier.get_tbd_url()
+                if not tbd_url:
+                    for worker in self.workers:
+                        if len(worker.myBackupList) != 0:
+                            print(worker.worker_id, "is still running")
+                            checkAll = True
+                        if checkAll == False:
+                            sleep(3)
+                    if checkAll == False:
                         for worker in self.workers:
                             if len(worker.myBackupList) != 0:
                                 print(worker.worker_id, "is still running")
                                 checkAll = True
-                            if checkAll == False:
+                            if checkAll != True:
                                 sleep(3)
-                        if checkAll == False:
-                            for worker in self.workers:
-                                if len(worker.myBackupList) != 0:
-                                    print(worker.worker_id, "is still running")
-                                    checkAll = True
-                                if checkAll != True:
-                                    sleep(3)
-                        if checkAll == False:
-                            break
-                    else:
-                        wID = self.urlID(tbd_url)
-                        if wID != self.worker_id:
-                            #Not my url, give to someone else
-                            self.workers[wID].addToMine(tbd_url)
-                            if self.workers[wID].running == False:
-                                self.workers[wID].running = True
-                        else:
-                            self.myBackupList.append(tbd_url)
+                    if checkAll == False:
+                        break
                 else:
-                    tbd_url = self.myBackupList.pop(len(self.myBackupList)-1) #Take url belonging to this worker
-                    try:
-                        resp = download(tbd_url, self.config, self.logger)
-                        self.logger.info(
-                            f"Downloaded {tbd_url}, status <{resp.status}>, "
-                            f"using cache {self.config.cache_server}.")
-                        scraped_urls = scraper(tbd_url, resp, self.wordCounts, self.uniqueURLs)
-                        for scraped_url in scraped_urls:
-                            self.frontier.add_url(scraped_url)
-                    except:
-                        print("Timeout error (5 seconds):",tbd_url)
-                    self.frontier.mark_url_complete(tbd_url)
-                    time.sleep(self.config.time_delay)
+                    wID = self.urlID(tbd_url)
+                    if wID != self.worker_id:
+                        #Not my url, give to someone else
+                        self.workers[wID].addToMine(tbd_url)
+                        if self.workers[wID].running == False:
+                            self.workers[wID].running = True
+                    else:
+                        self.myBackupList.append(tbd_url)
+            else:
+                tbd_url = self.myBackupList.pop(len(self.myBackupList)-1) #Take url belonging to this worker
+                try:
+                    resp = download(tbd_url, self.config, self.logger)
+                    self.logger.info(
+                        f"Downloaded {tbd_url}, status <{resp.status}>, "
+                        f"using cache {self.config.cache_server}.")
+                    scraped_urls = scraper(tbd_url, resp, self.wordCounts, self.uniqueURLs)
+                    for scraped_url in scraped_urls:
+                        self.frontier.add_url(scraped_url)
+                except:
+                    print("Timeout error (5 seconds):",tbd_url)
+                self.frontier.mark_url_complete(tbd_url)
+                time.sleep(self.config.time_delay)
 
     def addInfo(self,wordCounts,uniqueURLs,workers):
         self.wordCounts = wordCounts
